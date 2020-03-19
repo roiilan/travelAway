97551c681c894d244641eb5feacc89e490ecde20
roi.ilan <roiilan23@gmail.com>
2020-03-19 16:40:23 +0200

    first commit

---
+++ src/components/nav-bar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="nav-bar">
+      <router-link to="/">Home</router-link> 
+    </div>
+</template>
+
---
+++ src/services/favor.service.js
@@ -0,0 +1,9 @@
+const favotService = {
+    getEmptyFavor
+}
+
+function getEmptyFavor {
+    return {
+        
+    }
+}
