0ba32fe8f5a3e83d615cfc1e475a46cd52b1931a
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-03-19 17:07:50 +0200

    complite function getEmptyFavot in service

--- src/services/favor.service.js
+++ src/services/favor.service.js
@@ -2,8 +2,14 @@ const favotService = {
     getEmptyFavor
 }
 
-function getEmptyFavor {
+function getEmptyFavor() {
     return {
-        
+        _id: 'p1011',
+        title: 'Dog walking / Take my kids away / Picture on the side',
+        description: '',
+        requestedBy: {},
+        takenBy : {},
+        startAt: 2872222,
+        endsAt: null,
     }
 }
