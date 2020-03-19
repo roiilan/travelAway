221a0df4f8488d48576fa519277162df5ef50caa
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-03-19 17:58:27 +0200

    complite services (favor, eventbis, util, storage)

--- src/services/favor.service.js
+++ src/services/favor.service.js
@@ -1,5 +1,43 @@
-const favotService = {
-    getEmptyFavor
+import {utilService} from './util.service.js'
+import {storageService} from './storage.service.js'
+
+const KEY_FAVORS = 'favors';
+
+export const favorService = {
+    getEmptyFavor,
+    query,
+    save,
+    remove
+}
+
+function query(){
+    var favors =  storageService.load(KEY_FAVORS)
+    if (favors) return favors 
+    return _createFavors();
+}
+
+function save (favor) {
+    var favors = query()
+    favors = (favor._id)? _updateFavor(favor, favors): _addFavor(favor, favors);
+    storageService.store(KEY_FAVORS, favors)
+}
+
+function _updateFavor (currFavor, favors) {
+    const idx = favors.findIndex(favor=> favors._id = currFavor._id)
+    favors.splice(idx, 1, currFavor)
+    return favors
+}
+
+function _addFavor(currFavor, favors) {
+    favors.unshift(currFavor);
+    return favors
+}
+
+function remove(favorId){
+    var favors = query()
+    const idx = favors.findIndex(favor=> favors._id = favorId)
+    favors.splice(idx, 1)
+    storageService.store(KEY_FAVORS, favors)
 }
 
 function getEmptyFavor() {
@@ -10,6 +48,28 @@ function getEmptyFavor() {
         requestedBy: {},
         takenBy : {},
         startAt: 2872222,
+        endsAt: null,
+    }
+}
+
+function _createFavors(){
+
+    return [
+        _createFavor('Dog walking', 'Puki', Date.now - (1000 * 60 * 60 * 24 * 5)),
+        _createFavor('Take my kids away', 'Muki', Date.now - (1000 * 60 * 60 * 24 * 3)),
+        _createFavor('Picture on the side', 'Shuki', Date.now - (1000 * 60 * 60 * 24 * 30 * 2))
+    ]
+
+}
+
+function _createFavor(title, requestedBy, startAt){
+    return {
+        _id: utilService.makeId(),
+        title,
+        description: utilService.makeLorem(80),
+        requestedBy,
+        takenBy : {},
+        startAt,
         endsAt: null,
     }
 }
