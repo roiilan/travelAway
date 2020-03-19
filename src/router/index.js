b3e313967e27684833180b5a4f9f6c0f2016f117
roi.ilan <roiilan23@gmail.com>
2020-03-19 17:10:01 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import HomePage from '../views/Home.vue'
+import favorDetails from '../views/Home.vue'
+import favorList from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/favor',
+    name: 'favorList',
+    component: favorList
   },
   
+  {
+    path: '/favor/:id?',
+    name: 'favorDetails',
+    component: favorDetails
+  },
+  
+
 ]
 
 const router = new VueRouter({
