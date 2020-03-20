fd6d9c9433397637380786fb6c43401c51ba6e36
roi.ilan <roiilan23@gmail.com>
2020-03-20 16:06:42 +0200

    map feature

--- src/components/favor-map.vue
+++ src/components/favor-map.vue
@@ -1,23 +1,35 @@
 <template>
-<div>
-     Map
-     </div>
+  <GmapMap
+    :center="center"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    />
+  </GmapMap>
 </template>
-
 <script>
 
 
 export default {
   data() {
-    return { 
-    }
-  },
-  async created() {    
-  },
-}
+    return {
+      markers: [
+        { position: { lat: 33.088007, lng: 34.8031678 } },
+        { position: { lat: 33.088007, lng: 35.8031678 } }
+      ],
+      center:{ lat: 34.088007, lng: 34.8031678 }
+    };
+  }
+};
 </script>
 
 
-<style scoped>
-</style>
 
