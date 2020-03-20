ef1fa35ed9ab6268c9e9233668195fafed05dce1
roi.ilan <roiilan23@gmail.com>
2020-03-20 12:13:43 +0200

    hfghg

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,6 +1,17 @@
 <template>
+<div>
+
+<div>
+Logo
+</div>
+
     <div class="nav-bar">
       <router-link to="/">Home</router-link> 
     </div>
+
+<div>
+Hello guest
+</div>
+</div>
 </template>
 
