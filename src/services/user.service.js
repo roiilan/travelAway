8eeb5db1093f78fc796c5cc6ba902c01c4a893f6
Your NameTamir <tamirn7@gmail.com>
2020-03-23 15:27:55 +0200

    try without inon

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
-import {utilService} from './util.service.js'
-import {storageService} from './storage.service.js'
-import {locService} from './loc.service.js'
+import { utilService } from './util.service.js'
+import { storageService } from './storage.service.js'
+import { locService } from './loc.service.js'
 // import httpService from './http.service'
 
 const KEY_USERS = 'users';
@@ -21,12 +21,13 @@ export const userService = {
 
 async function getById(userId) {
     var users = getUsers();
-    return users.find(user=> user._id === userId);
+    return users.find(user => user._id === userId);
     // return httpService.get(`user/${userId}`)
 }
+
 function remove(userId) {
     var users = getUsers();
-    const idx = users.findIndex(user=> user._id === userId);
+    const idx = users.findIndex(user => user._id === userId);
     users.splice(idx, 1);
     storageService.store(KEY_USERS, users);
     return ('Deletion of the browser successfully completed!!');
@@ -35,7 +36,7 @@ function remove(userId) {
 
 function update(currUser) {
     var users = getUsers();
-    const idx = users.findIndex(user=> user._id === currUser._id);
+    const idx = users.findIndex(user => user._id === currUser._id);
     users.splice(idx, 1, currUser);
     storageService.store(KEY_USERS, users);
     return currUser;
@@ -44,23 +45,23 @@ function update(currUser) {
 
 async function login(credentials) {
     var users = getUsers();
-    var user = users.find(user=> user.username.toUpperCase() === credentials.username.toUpperCase() && user.password === credentials.password);
-    if (user){
+    var user = users.find(user => user.username.toUpperCase() === credentials.username.toUpperCase() && user.password === credentials.password);
+    if (user) {
         sessionStorage.setItem(KEY_LOGGEDIN, JSON.stringify(user))
         return user
     } else return ('User Not Found')
-    // const user = await httpService.post('auth/login', userCred)
-    // return _handleLogin(user)
+        // const user = await httpService.post('auth/login', userCred)
+        // return _handleLogin(user)
 }
 async function signup(newUserCred) {
     sessionStorage.clear();
     var users = getUsers();
     newUserCred._id = utilService.makeId();
-    newUserCred.joinAt = {date:_getValidDate (new Date()),time: _getValidtime(new Date())};
+    newUserCred.joinAt = { date: _getValidDate(new Date()), time: _getValidtime(new Date()) };
     newUserCred.karma = 5;
     console.log('newUserCred before: ', newUserCred);
     var pos = await locService.getPosition()
-    newUserCred.position = {lat: pos.coords.latitude, lng: pos.coords.longitude}
+    newUserCred.position = { lat: pos.coords.latitude, lng: pos.coords.longitude }
     console.log('newUserCred after: ', newUserCred);
     users.push(newUserCred)
     storageService.store(KEY_USERS, users)
@@ -89,20 +90,21 @@ async function logout() {
     sessionStorage.clear();
     return ('Exit successfully completed')
 }
+
 function getUsers() {
-    var users =  storageService.load(KEY_USERS);
-    if (users) return users 
+    var users = storageService.load(KEY_USERS);
+    if (users) return users
     return _createUsers();
     // return httpService.get('user')
 }
 
 function getLoggeinUser() {
-    return JSON.parse(sessionStorage.getItem(KEY_LOGGEDIN))    
+    return JSON.parse(sessionStorage.getItem(KEY_LOGGEDIN))
 }
 
-function getMinimalUser(user){
+function getMinimalUser(user) {
     return {
-        _id:user._id,
+        _id: user._id,
         fullName: user.fullName,
         imgUrl: user.imgUrl
     }
@@ -118,47 +120,41 @@ function getEmptyUser() {
     }
 }
 
-function _createUsers(){
+function _createUsers() {
     var users = [
         _createUser(
-         'u101',
-         'Muki',
-         '111',
-         'Muki Ben Moshe',
-         'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-          15,
-          { lat:12, lng: 12 },
-          {date:'2020-01-14', time:'8:32'},
-          true
-          ),
+            'u101',
+            'Muki',
+            '111',
+            'Muki Ben Moshe',
+            'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+            15, { lat: 12, lng: 12 }, { date: '2020-01-14', time: '8:32' },
+            true
+        ),
         _createUser(
             'u102',
             'Puki',
             '222',
             'Puki Ben Pinhas',
             'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-            8,
-            { lat:22, lng: 21 },
-            {date:'2020-02-27', time:'17:47'},
+            8, { lat: 22, lng: 21 }, { date: '2020-02-27', time: '17:47' },
             false
-            ),
+        ),
         _createUser(
-         'u103',
-         'Shuki',
-         '333',
-         'Shuki Ben Shaul',
-         'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-         44,
-        { lat:13, lng: 31 },
-        {date:'2019-01-30', time:'12:12'},
-        false
+            'u103',
+            'Shuki',
+            '333',
+            'Shuki Ben Shaul',
+            'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+            44, { lat: 13, lng: 31 }, { date: '2019-01-30', time: '12:12' },
+            false
         )
-       ]
+    ]
     storageService.store(KEY_USERS, users)
     return users;
 }
 
-function _createUser(_id, username, password, fullName, imgUrl, karma, position, joinAt, isAdmin){
+function _createUser(_id, username, password, fullName, imgUrl, karma, position, joinAt, isAdmin) {
     return {
         _id,
         username,
@@ -167,16 +163,10 @@ function _createUser(_id, username, password, fullName, imgUrl, karma, position,
         imgUrl,
         karma,
         position,
-<<<<<<< HEAD
-        // zoomUrl: '',
-        // projsAsked: [],
-        // projsGivven : [],
-=======
         // pref:[], לעשות בסוף
         // zoomUrl: '', לעשות בסוף
         // favorsAsked: [],
         // favorsGivven : [],
->>>>>>> d177723f13ed8f243d441d991db6f8768a4cf874
         joinAt,
         isAdmin
     }
@@ -188,14 +178,14 @@ function _createUser(_id, username, password, fullName, imgUrl, karma, position,
 //     return user;
 // }
 
-function _getValidDate (date){
+function _getValidDate(date) {
     return date.getFullYear() + '-' + _padNum((date.getMonth() + 1)) + '-' + _padNum(date.getDate());
 }
 
-function _getValidtime (time){
+function _getValidtime(time) {
     return _padNum(time.getHours()) + ':' + _padNum(time.getMinutes());
 }
 
-function _padNum(num){
-    return (num < 10)? '0' + num: num;
+function _padNum(num) {
+    return (num < 10) ? '0' + num : num;
 }
