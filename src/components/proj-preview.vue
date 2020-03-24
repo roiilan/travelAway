f624152a6d95b2f8d75afaa20ab4bff826719b73
Your NameTamir <tamirn7@gmail.com>
2020-03-24 20:46:40 +0200

    good night push

--- src/components/proj-preview.vue
+++ src/components/proj-preview.vue
@@ -4,10 +4,17 @@
 
     <img v-bind:src=proj.imgUrls[0] v-if="proj.imgUrls" class="details-img"/>
     <div class="proj-txt">
+      <div class="proj-txt-header">
     <h5>{{projCategory}}</h5>
     <h2>{{proj.title}} </h2>
     </div>
-
+    <div class = "members-needed">
+     <h5> Members Needed:</h5>
+    <div class = "user-img" v-for="i in proj.membersNeeded">
+       <img src="../../img/voulnteer.png"/>
+    </div>
+</div>
+    </div>
     <div class = "footer">
     </div>
       </router-link> 
@@ -16,7 +23,6 @@
 
 
 <script>
-
 export default {
   name: 'projPreview',
   props: {
@@ -34,6 +40,7 @@ export default {
 </script>
 
 <style scoped >
+
 
 
 </style>
