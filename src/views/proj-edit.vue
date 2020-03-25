bad6415ab0383304e3a813292d0302b96d07004b
Your NameTamir <tamirn7@gmail.com>
2020-03-25 18:20:53 +0200

    push

--- src/views/proj-edit.vue
+++ src/views/proj-edit.vue
@@ -3,11 +3,11 @@
         <form class="form-proj-edit flex col" @submit.prevent="save(proj)">
           <div class="flex around">
             <div class="flex col">
-              <!-- <label class="proj-edit-upload-img"> 
+              <label class="proj-edit-upload-img"> 
                   <input @input="uploadImg" type="file" hidden>
                   <img src="https://image.flaticon.com/icons/svg/1837/1837526.svg">
                   <p class="proj-edit-upload-txt">Upload! </p>
-              </label> -->
+              </label>
               
             <div v-for="(url, index) in proj.imgUrls" :key="url" class="img-proj-container ratio-16-9">
                 <label > 
