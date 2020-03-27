0ee0d025e69c927b6a86d8be7001a1a561cc2e91
roi.ilan <roiilan23@gmail.com>
2020-03-27 11:20:53 +0300

    map features

--- src/services/map.service.js
+++ src/services/map.service.js
@@ -12,9 +12,7 @@ import axios from "axios";
 async function searchPosition(txt){
     console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${txt}&key=${GAPI_KEY}&language=en&region=US`,'mapService ',txt, )
     const res= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${txt.split(" ").join('+')}&key=${GAPI_KEY}&language=en&region=US`)
-    // console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${txt}&key=${GAPI_KEY}`,'mapService ',txt, )
-    // const res= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${txt.split(" ").join('+')}&key=${GAPI_KEY}`)
-         
+             
     try { 
         var position=res.data.results[0].geometry.location ;
         const address_components_Length =res.data.results[0].address_components.length;   
@@ -25,13 +23,23 @@ for (var i=0;i<address_components_Length;i++){
         if(results[i].types[j]==='country'){
             var country=results[i].long_name;
             var short_country =results[i].short_name; //short name country          
-        }      
+        }   
+        if(results[i].types[j]==='locality'){
+            var city=results[i].long_name;
+            // var shortCity =results[i].short_name; //short name city   
+        }   
+        
+        if(results[i].types[j]==='administrative_area_level_1')
+        {
+            var region=results[i].long_name;                    
+        }   
+
     }
-}       
-        //  console.log(country,'country')
-        //  console.log(short_country,'short_country')
+}        
         position.country=country;
         position.short_country=short_country;
+        position.city=city;
+        position.region=region;
         console.log(position)
          return position    
            
