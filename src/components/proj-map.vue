a8be0701d10bcffe03f5bf86429565a3ff3c79af
Yinon_Eliyahu <yinoneliyaoo@gmail.com>
2020-03-30 09:23:12 +0300

    addingim to map

--- src/components/proj-map.vue
+++ src/components/proj-map.vue
@@ -1,48 +1,107 @@
 <template>
-  <GmapMap
-     v-if="position"
-    :center="position"
-    :zoom="zoomSize"
-    map-type-id="terrain"
-  >
+<div class="container-map">
+  {{markerOptions.url}}
+  <transition name="fade">
+  <img 
+  class="img-on-map" 
+  v-if="isShow" :src="url" 
+  alt="" 
+  :style="`top:${y}px; left:${x}px`">
+  <!-- <img 
+  @mouseover="holdImg"
+  @mouseout="closeImg" 
+  class="img-on-map" 
+  v-if="isShow" :src="url" 
+  alt="" 
+  :style="`top:${y}px; left:${x}px`"> -->
+  </transition>
+  <GmapMap v-if="position" style="height: 500px" :center="position" :zoom="zoomSize" map-type-id="terrain">
     <!-- style="height: 500px" -->
     <GmapMarker
       :key="index"
       v-for="(m, index) in markers"
+      :ref="`marker${index}`"
       :position="m.position"
-      :clickable="false"
+      :clickable="true"
       :draggable="false"
-      @click="center=m.position"
+      :icon="markerOptions"
+      @mouseover="openImg(m, index, $event)"
+      @mouseout="closeImg"
+      @click="openDetails(m)"
     />
+    <!-- <GmapMarker
+      class="marker-hover"
+      :position="pos"
+      :clickable="true"
+      :draggable="false"
+      :icon="markerlight"
+      @mouseover="holdImg"
+      @mouseout="closeImg"
+
+    /> -->
   </GmapMap>
+</div>
 </template>
 <script>
+const mapMarker = require("../assets/icon/airport.png");
+const lightMarker = require("../assets/icon/airport-right.png");
+
 
 export default {
-   props: {
-   position: null,
-      markers: Array,
-      zoomSize:null
+  props: {
+    position: null,
+    markers: Array,
+    zoomSize: null
   },
-    data() {
+  data() {
     return {
+      markerOptions: {
+        url: lightMarker,
+        size: { width: 60, height: 90, f: "px", b: "px" },
+        scaledSize: { width: 40, height: 45, f: "px", b: "px" }
+      },
+      // markerlight: {
+      //   url: mapMarker,
+      //   size: { width: 60, height: 90, f: "px", b: "px" },
+      //   scaledSize: { width: 40, height: 45, f: "px", b: "px" }
+      // },
+      url: null,
+      x: 0,
+      y: 0,
+      isShow: false,
+      // pos: {lat:34,lng: 34},
     };
   },
-  created(){
-    // console.log(this.position.lat);
+  created() {
   },
-
+ 
+  methods: {
+    openImg(m, index ,$event) {
+      this.x = $event.tb.pageX
+      this.y = $event.tb.pageY
+      this.isShow = true
+      // this.pos = m.position 
+      this.url = m.url
+    },
+    // holdImg(){
+    //   this.isShow = true
+    // },
+    closeImg() {
+      this.isShow = false
+    },
+    openDetails(m){
+      this.$router.push('/proj/' + m.id)
+    }
+  }
 };
-
-
-
-
-
-      // markers: [
-      //   // { position: { lat: this.position.lat, lng: this.position.lng } },     
-      // ],
-      // center: { lat: this.position.lat, lng: this.position.lng }
 </script>
+
+<style>
+.marker-hover{
+  /* z-index: 1200; */
+}
+
+</style>
 
 
 
--- src/components/side-bar.vue
+++ src/components/side-bar.vue
@@ -1,54 +1,52 @@
 <template>
   <div class="side-bar">
-      <div class="map">
-    <proj-map :zoomSize="zoomSize" :markers="markers" :position="position"> </proj-map>
-  </div>
+    <div class="map">
+      <proj-map :zoomSize="zoomSize" :markers="markers" :position="position"></proj-map>
+    </div>
   </div>
 </template>
 
 <script>
-
-import projMap from './proj-map.vue';
+import projMap from "./proj-map.vue";
 
 export default {
-      props: {
-   projs: Array,
+  props: {
+    projs: Array
   },
-      data() {
+  data() {
     return {
-      zoomSize:1.5,
-      markers:[],
-      position:{
-       txtAdress:null,
-       lat:33.886917,
-       lng:9.537499
-   }
-   }
+      zoomSize: 1.5,
+      markers: [],
+      position: {
+        txtAdress: null,
+        lat: 33.886917,
+        lng: 9.537499
+      }
+    };
   },
 
-    components:{
-        projMap
-    },
-    methods:{
-        getMarkers(){
-           var markers = []
-           this.projs.map(proj=>{       
-             markers.push(proj.position)
-            }),
-            markers.map(marker=>{
-                var position = new Object();  
-                this.markers.push({ position: { lat: marker.lat, lng: marker.lng } })
-            })
-        }
-    },
-    created(){
-        this.getMarkers()  
+  components: {
+    projMap
+  },
+  methods: {
+    getMarkers() {
+      var markers = [];
+      this.projs.map(proj => {
+        // var pos = new Object();
+        markers.push({pos: proj.position, url: proj.imgUrls[0], id: proj._id});
+      }),
+        markers.map(marker => {
+          // var position = new Object();
+          this.markers.push({ position: { lat: marker.pos.lat, lng: marker.pos.lng }, url: marker.url, id: marker.id });
+        });
     }
-
-}
+  },
+  created() {
+    this.getMarkers();
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
 
