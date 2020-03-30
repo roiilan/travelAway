a2aa50c148795d006dcbf09e1c7ab6aab2fb7dcd
roi.ilan <roiilan23@gmail.com>
2020-03-30 22:13:29 +0300

    fix list search

--- src/components/proj-list-search.vue
+++ src/components/proj-list-search.vue
@@ -1,28 +1,23 @@
 <template>
 
-<div>
-
-</div>
-
   <div class="proj-list" v-if="projs" >
    <h1>BE THE CHANGE. BE A GLOBAL VOLUNTEER ABROAD.</h1>
    <h2 v-if="noRes">There is no result</h2>
    <ul class="around-the-world-list">
       <li v-for="proj in projs" :key="proj._id" class="list-card" >
-
-<router-link :to="'/proj/' + proj._id">
-        <div class="list-projs-img-conatainer ratio-card">
-        <img :src="proj.imgUrls[0]" class="proj-preview-img" >
-        <div class = "list-projs-country-details">
-        <img :src="`https://www.countryflags.io/${proj.position.short_country}/shiny/64.png`" v-if="proj.position.short_country" class="world-proj-flag">
-        <h5 v-if="proj.position.city">{{proj.position.city}},</h5>
-        <h5 v-else>{{proj.position.region}},</h5>
-        <h5>{{proj.position.country}}</h5>
-            </div>
+        <router-link :to="'/proj/' + proj._id">
+          <div class="list-projs-img-conatainer ratio-card">
+          <img :src="proj.imgUrls[0]" class="proj-preview-img" >
+          <div class = "list-projs-country-details">
+          <img :src="`https://www.countryflags.io/${proj.position.short_country}/shiny/64.png`" v-if="proj.position.short_country" class="world-proj-flag">
+          <h5 v-if="proj.position.city">{{proj.position.city}},</h5>
+          <h5 v-else>{{proj.position.region}},</h5>
+          <h5>{{proj.position.country}}</h5>
+        </div>
         </div>
         <h5>{{proj.title}}</h5>
         <h6>{{proj.description.substring(0,80) +'...'}}</h6>
-</router-link>
+        </router-link>
       </li>
     </ul>
   </div>
@@ -31,8 +26,7 @@
 
 
 <script>
-import projPreview from "./proj-preview";
-// import projSearch from "./proj-search.vue";
+import projPreview from "./proj-preview.vue";
 
 export default {
   name: "projsListSearch",
@@ -67,12 +61,12 @@ export default {
       filteredProjs = this.projs.filter(proj=>{ 
         if (proj.position.country) {
         return proj.title.toLowerCase().includes(txtFilter.toLowerCase())||
-               proj.description.toLowerCase().includes(txtFilter.toLowerCase()) || 
-               proj.position.country.toLowerCase().includes(txtFilter.toLowerCase())
+               proj.description.toLowerCase().includes(txtFilter.toLowerCase())  
+              || proj.position.country.toLowerCase().includes(txtFilter.toLowerCase())
         }       
          return proj.title.toLowerCase().includes(txtFilter.toLowerCase())||
                 proj.description.toLowerCase().includes(txtFilter.toLowerCase())
-        //      || proj.position.city.toLowerCase().includes(txtFilter.toLowerCase())
+       // //      || proj.position.city.toLowerCase().includes(txtFilter.toLowerCase())
 
      })
    }
@@ -93,7 +87,6 @@ export default {
   },
     components: {
     projPreview,
-    // projSearch
   }
 };
 </script>
