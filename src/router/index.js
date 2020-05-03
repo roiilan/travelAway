8cd586a2c682a8d0904c3560dd2610d02e8d4519
roi.ilan <roiilan23@gmail.com>
2020-05-03 17:17:56 +0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import projApp from '../views/walkways-app.vue'
-import Login from '../views/Login.vue';
-import userProfile from '../views/user-profile.vue'
-import userEdit from '../views/user-edit.vue'
-import projEdit from '../views/proj-edit.vue'
-import projListFiltered from '../views/proj-list-filtered.vue'
-import projDetails from '../components/proj-details.vue'
-import projList from '../components/proj-list.vue'
-import projSearch from '../components/proj-search.vue'
-import projListSearch from '../components/proj-list-search.vue'
+import projApp from '../pages/walkways-app.vue'
+import Login from '../pages/Login.vue';
+import userProfile from '../pages/user/user-profile.vue'
+import userEdit from '../pages/user/user-edit.vue'
+import projEdit from '../pages/proj/proj-edit.vue'
+import projListFiltered from '../pages/proj/proj-list-filtered.vue'
+import projDetails from '../pages/proj/proj-details.vue'
+import projList from '../pages/proj/proj-list.vue'
+// import projSearch from '../components/proj-search.vue'
+// import projListSearch from '../components/proj-list-search.vue'
+
 
 Vue.use(VueRouter)
 
@@ -23,11 +24,11 @@ const routes = [{
         name: 'aroundTheWorld',
         component: projList
     },
-    {
-        path: '/search',
-        name: 'projSearch',
-        component: projSearch
-    },
+    // {
+    //     path: '/search',
+    //     name: 'projSearch',
+    //     component: projSearch
+    // },
     // {
     //     path: '/projects/:querys?',
     //     name: 'projListSearch',
@@ -58,11 +59,11 @@ const routes = [{
         name: 'projEdit',
         component: projEdit
     },
-    {
-        path: '/proj/:id?',
-        name: 'projDetails',
-        component: projDetails
-    },
+    // {
+    //     path: '/proj/:id?',
+    //     name: 'projDetails',
+    //     component: projDetails
+    // },
 ]
 
 const router = new VueRouter({
